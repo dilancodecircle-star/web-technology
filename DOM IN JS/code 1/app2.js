cddf478ecf64d078441ead91d0ba7c99dc11f04a
dilancodecircle-star <219716051+Dilancode-circle@users.noreply.github.com>
2026-09-19 17:56:54 +0530

    check parent element

--- DOM IN JS/code 1/app2.js
+++ DOM IN JS/code 1/app2.js
@@ -1,3 +1,6 @@
-let ul = document.querySelector('ul');
+/*let ul = document.querySelector('ul');
 console.log(ul.parentNode.parentNode);
-console.log(ul.parentElement);
+console.log(ul.parentElement);*/
+
+let html = document.documentElement ;
+console.log(html.parentElement);
