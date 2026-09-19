03a5929b47c62f8d17f43e3a67dc454de5037023
dilancodecircle-star <219716051+Dilancode-circle@users.noreply.github.com>
2026-09-19 19:00:46 +0530

    get data in form

--- DOM IN JS/code2/app.js
+++ DOM IN JS/code2/app.js
@@ -1,4 +1,4 @@
-let titleElement = document.getElementById('title');
+/*let titleElement = document.getElementById('title');
 console.log(titleElement);
 
 let buttonElement = document.getElementById('myButton');
@@ -10,4 +10,14 @@ buttonElement.style.color ='blue';
 buttonElement.addEventListener('click' , function(){
     titleElement.innerText = "you clicked the button";
     document.body.style.backgroundColor = "lightblue";
-});
+});*/
+
+const formElement = document.getElementById('myForm');
+formElement.addEventListener('submit' , function(event){
+    event.preventDefault();
+    const nameValue = document.getElementById('username').value;
+    const emailValue = document.getElementById('useremail').value;
+
+    console.log("user nam : " + nameValue);
+    console.log("usr email : " + emailValue);////////////////////////////////
+})
