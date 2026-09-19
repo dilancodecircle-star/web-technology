e6a1bd4bbac084b013612fa0a5fb2943fcd3e612
dilancodecircle-star <219716051+Dilancode-circle@users.noreply.github.com>
2026-09-19 18:00:28 +0530

    child node

--- DOM IN JS/code 1/app2.js
+++ DOM IN JS/code 1/app2.js
@@ -2,5 +2,5 @@
 console.log(ul.parentNode.parentNode);
 console.log(ul.parentElement);*/
 
-let html = document.documentElement ;
-console.log(html.parentElement);
+let ul = document.querySelector('ul');
+console.log(ul.hasChildNodes);
