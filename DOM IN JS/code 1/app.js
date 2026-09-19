2335c540c61d7abd0b15f6c55107e905cb5b9cb9
dilancodecircle-star <219716051+Dilancode-circle@users.noreply.github.com>
2026-09-19 17:05:11 +0530

    create elements and adding content for it

--- DOM IN JS/code 1/app.js
+++ DOM IN JS/code 1/app.js
@@ -1,8 +1,14 @@
 /*const myListItem = document.querySelector(".list-items");
 //for(var i= 0; i<myListItem.length ; i++){
     myListItem.style.color = 'red';
-//}
+//}*/
 
 const ul = document.querySelector('ul');
 const li = document.createElement('li');
-ul.append(li); */
+ul.append(li); 
+
+const  firstListItem = document.querySelector('.list-items');
+console.log(firstListItem.innerText)
+console.log(firstListItem.textContent)
+console.log(firstListItem.innerHTML);
+li.innerText = 'x-man';
