e00a61d089c11675a05e51656c8db43852b3d69f
dilancodecircle-star <219716051+Dilancode-circle@users.noreply.github.com>
2026-09-21 22:32:27 +0530

    modify data

---
+++ lab 04/javascript.js
@@ -0,0 +1,9 @@
+console.log("The external javascript file");
+
+console.log("This output appers in the browser console");
+alert("this apper in a alert box");
+
+document.getElementById("output-message").innerHTML="this message apper inside the web";
+
+let today = new DataTransfer();
+ document.getElementById("current-data").innerHTML = "current date and time : "+ today;
--- lab 04/script.js
+++
@@ -1,4 +0,0 @@
-let today =new Date();
-
-document.getElementById("current-date").innerHTML=
-    "Current date and time :" + today;
